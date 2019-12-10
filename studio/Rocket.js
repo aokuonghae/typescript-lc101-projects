445780cf19d0322a16051095bf2649522485c4f4
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-09 20:15:31 -0600

    Completed Exercises and studio

---
+++ studio/Rocket.js
@@ -0,0 +1,45 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+        this.cargoItems = [];
+        this.astronauts = [];
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sum = items.reduce(function (acc, val) { return acc + val.massKg; }, 0);
+        return sum;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if ((this.currentMassKg() + item.massKg) <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
